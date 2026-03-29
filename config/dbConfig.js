bccf8dbe78cb1049459a45ca88c288f120329005
Manish Pokhrel <manishpokhrel098@gmail.com>
2026-03-29 20:16:11 +1100

    fix server

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,42 @@
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "postgres",
+//   protocol: "postgres",
+//   logging: false,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false, // important for Neon
+//     },
+//   },
+// });
+
+// export default sequelize;
+
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // important for Neon
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // needed for Neon / serverless
+      },
     },
-  },
-});
+  }
+);
 
 export default sequelize;

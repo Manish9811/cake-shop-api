06a04fad65753fd8c224ff2f8dcc04ceb7692ad3
Manish Pokhrel <manishpokhrel098@gmail.com>
2026-03-29 19:36:57 +1100

    fix config file

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,19 +2,21 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log("url" + process.env.DATABASE_URL)
+
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  protocol: "postgres",
   logging: false,
 
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // important for Neon
-    },
-  },
+  dialectOptions:
+    process.env.NODE_ENV === "production"
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false, // needed for Neon
+          },
+        }
+      : {},
 });
 
 export default sequelize;
